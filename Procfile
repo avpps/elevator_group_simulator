330589239bf160558128209abb04ab8a80d79c61
Piotr Pszonka <piotrallegr@gmail.com>
2017-06-07 15:42:08 +0200

    static _root update

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-heroku config:set DISABLE_COLLECTSTATIC=1
 web:python manage.py runserver
--- groupsimulator/settings.py
+++ groupsimulator/settings.py
@@ -120,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
