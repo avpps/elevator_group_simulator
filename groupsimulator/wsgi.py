60965cb09ef2ebc12158725527d28808a60767c8
Piotr Pszonka <piotrallegr@gmail.com>
2017-06-08 17:34:22 +0200

    a

--- groupsimulator/wsgi.py
+++ groupsimulator/wsgi.py
@@ -13,8 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "groupsimulator.settings")
 
-<<<<<<< HEAD
+
 application = get_wsgi_application()
-=======
-application = get_wsgi_application() 
->>>>>>> d6ef397b75d5ef7882bff7f59fb0ade5740d3a9b
