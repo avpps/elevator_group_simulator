7a97bae570946a71cacb85ce78b10ee01175e51c
Piotr Pszonka <piotrallegr@gmail.com>
2017-06-09 08:24:57 +0200

    -

--- groupsimulator/settings.py
+++ groupsimulator/settings.py
@@ -87,6 +87,15 @@ DATABASES = {
     }
 }
 
+''' for alternative use sqlite3 database 'db.sqlite3' attached in main dir:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
