9c87dbd9c37a925b3ec69f33a1c8e573dbb40a79
Piotr Pszonka <piotrallegr@gmail.com>
2017-06-09 08:10:56 +0200

    db change to Postgresql

--- groupsimulator/settings.py
+++ groupsimulator/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'groupsimulator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maingroupdb',
+        'USER': 'postgres',
+        'PASSWORD': 'q',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
